1760fb553953a5967ea407d921e49114c8ad36fa
MariaLuiza <maria.l.costa46@aluno.senai.br>
2025-06-18 12:15:05 -0300

    Incluindo Nota3

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ function calcularMédia() {
     //Pega os valores dos campos
     var n1 = parseFloat(document.getElementById('nota1').value);
     var n2 = parseFloat(document.getElementById('nota2').value);
+    var n3 = parseFloat(document.getElementById('nota3').value);
 
     // Verifica se são números válidos
-    if (isNaN(n1) || isNaN(n2)) {
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
         alert("Por favor, insira notas válidas!");
         return;
     }
    
     // Calcula a média
-    var media = (n1 + n2) / 2;
+    var media = (n1 + n2 + n3) / 3;
  
     // Mostra o resultado
     document.getElementById('resultado').innerText = "A média é: " + media.toFixed(2);
